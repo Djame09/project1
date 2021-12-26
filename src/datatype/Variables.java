87a7499f7d3ba6144297f9f652968f240c27fe84
Djamel09 <djamelnaitallou@gmail.com>
2021-12-25 21:30:44 -0500

    first commit

---
+++ src/datatype/Variables.java
@@ -0,0 +1,16 @@
+package datatype;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		System.out.println ("--------------------Welcom--------------------");
+		System.out.println("--Celsius-Fahrenheit/Fahrenheit-celsius converter--");
+		System.out.println("choose the conversion mode:");
+	
+		
+		 
+			
+
+	}
+
+}
